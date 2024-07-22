63cd8dfe565e8a6312a8d39923c8320fdd88a7a9
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-07-22 11:54:20 +0200

    Update main.ts

--- app/main.ts
+++ app/main.ts
@@ -6,10 +6,13 @@ import * as net from "net";
 console.log("Logs from Test Program will appear here!");
 
 const server = net.createServer((socket) => {
-  socket.on('data', (data) => {
+  socket.on("data", (data) => {
     const request = data.toString();
-    const path = request.split(' ')[1];
-    const response = path === '/' ? 'HTTP/1.1 200 OK\r\n\r\n' : 'HTTP/1.1 404 Not Found\r\n\r\n';
+    const path = request.split(" ")[1];
+    const response =
+      path === "/"
+        ? "HTTP/1.1 200 OK\r\n\r\n"
+        : "HTTP/1.1 404 Not Found\r\n\r\n";
     socket.write(response);
     socket.end();
   });
@@ -17,3 +20,4 @@ const server = net.createServer((socket) => {
   server.listen(4221, "localhost", () => {
     console.log("Server is running on port 4221");
   });
+});

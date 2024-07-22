1a21d661efb5e255faa17465766c96ffc202b350
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-07-22 12:02:13 +0200

    Update main.ts
    Restructure file

--- app/main.ts
+++ app/main.ts
@@ -2,22 +2,31 @@
 
 import * as net from "net";
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from Test Program will appear here!");
-
 const server = net.createServer((socket) => {
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+
   socket.on("data", (data) => {
     const request = data.toString();
+
     const path = request.split(" ")[1];
+
     const response =
       path === "/"
         ? "HTTP/1.1 200 OK\r\n\r\n"
         : "HTTP/1.1 404 Not Found\r\n\r\n";
+
     socket.write(response);
+
     socket.end();
   });
+});
 
-  server.listen(4221, "localhost", () => {
-    console.log("Server is running on port 4221");
-  });
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+
+console.log("Logs from your program will appear here!");
+
+// Uncomment this to pass the first stage
+
+server.listen(4221, "localhost", () => {
+  console.log("Server is running on port 4221");
 });

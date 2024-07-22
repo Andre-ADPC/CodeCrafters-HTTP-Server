75c6396ad6d3b145b9394e7ff91d037a6885ee25
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-07-22 22:15:16 +0200

    Use an Intervention Test File

--- app/main.ts
+++ app/main.ts
@@ -3,63 +3,61 @@
 import * as net from "net";
 import { readFileSync, statSync } from "fs";
 
-// Closing the Socket
+// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
 
-  // Openning the Socket
+  // Opening the Socket
 
   socket.on("data", (data) => {
-    const request = data.toString();
-    const path = request.split(" ")[1];
-    const content_type = "text/plain";
+    const req = data.toString();
+    const path = req.split(" ")[1];
 
-    // Updated Response
+    // Default Response
     let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    //Conditional Responses
     if (path === "/") {
       response = "HTTP/1.1 200 OK\r\n\r\n";
     }
-
     if (path.startsWith("/echo/")) {
-      const resp_body = path.slice("/echo/".length);
+      const str = path.slice("/echo/".length);
       // console.log(path);
-      // console.log(resp_body);
-      response = `HTTP/1.1 200 OK\r\nContent-Type: ${content_type}\r\nContent-Length: ${resp_body.length}\r\n\r\n${resp_body}`;
+      // console.log(str);
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`;
     }
 
-    // Testing for User-Client
-    if (path === "/User-Agent") {
-      const resp_body = request
+    // Testing for User-Agent
+    if (path === "/user-agent") {
+      const str = req
         .split("\r\n")
-        .find((el) => el.toLowerCase().includes("User-Agent:"))!
-        .slice("User-Agent:".length)
+        .find((el) => el.toLowerCase().includes("user-agent:"))!
+        .slice("user-agent:".length)
         .trim();
-      console.log("request", request.split("\r\n"));
-      console.log("resp_body", resp_body);
-      response = `HTTP/1.1 200 OK\r\nContent-Type: ${content_type}\r\nContent-Length: ${resp_body.length}\r\n\r\n${resp_body}`;
+      // console.log("req",req.split('\r\n'));
+      // console.log("str",str);
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`;
     }
 
     // Testing for Files
     if (path.startsWith("/files/")) {
-      const resp_body = request;
-      const fileName = path.slice("/files/".length);
-      //const fileSize = statSync("tmp/" + fileName).size;
+      const fileName = path.slice("/files/".length).trim();
+      // const fileSize = statSync('tmp/'+fileName).size;
       const fileSize = statSync(process.argv[3] + fileName).size;
-      response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileName}\r\n\r\n${resp_body}`;
+      const fileContent = readFileSync(process.argv[3] + fileName);
+      response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileSize}\r\n\r\n${fileContent}`;
     }
-
     socket.write(response);
     socket.end();
   });
 });
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 
+// You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
+// Initial Stage
 
 server.listen(4221, "localhost", () => {
   console.log("Server is running on port 4221");

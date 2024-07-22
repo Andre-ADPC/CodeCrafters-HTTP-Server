9cc1779970b742f98bfe18fd00a6034f3086fb93
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-07-22 13:48:45 +0200

    Update main.ts
    Stage 3 Updates

--- app/main.ts
+++ app/main.ts
@@ -1,21 +1,34 @@
 /** @format */
 
 import * as net from "net";
-
+// Closing the Socket
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("close", () => {
+    socket.end();
+  });
+
+// Writing to Socket
+const server = net.createServer((socket) => {
+  // socket.write("HTTP/1.1 200 OK\r\n\r\n");
 
   socket.on("data", (data) => {
     const request = data.toString();
-
     const path = request.split(" ")[1];
+    //  const response =
+    //    path === "/" ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 Not Found\r\n\r\n";
+    //  socket.write(response);
 
-    const response =
-      path === "/" ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 Not Found\r\n\r\n";
+    // Updated Response
+    let response = 'HTTP/1.1 404 Not Found\r\n\r\n';
+    if (path === '/') {
+      response = 'HTTP/1.1 200 OK\r\n\r\n';
+    }
+    if (path.startsWith('/echo/')) {
+      response = `HTTP/1.1 200 OK\r\nContent-Type: Plain Text\r\nContent-Length: 3\r\n\r\n${path.split('/echo/')}`;
+    }
     socket.write(response);
-
     socket.end();
-  })
+  });
 });
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.

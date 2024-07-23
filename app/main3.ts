6a23bbd2801af59d7224f92a07e74ad695f7f3f1
Andre Du Plessis <andre.du.plessis@adpc-llc.com>
2024-07-23 22:39:01 +0200

    Update main3.ts
    Move earlier version file contents

--- app/main3.ts
+++ app/main3.ts
@@ -1,55 +1,49 @@
 /** @format */
-import * as net from "net";
-import { readFileSync } from "fs";  // Refactor?
-import { readFile } from "fs/promises";
 
-// Terminating the Socket
+import * as net from "net";
+import { readFileSync, statSync } from "fs";
+
+// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
 
   // Opening the Socket
-  socket.on("data", async (data) => {
+
+  socket.on("data", (data) => {
     const request = data.toString();
-    const method = request.split(" ")[0];
     const path = request.split(" ")[1];
     const content_type = "text/plain";
-
     // Default Response
     let response = "HTTP/1.1 404 Not Found\r\n\r\n";
-    //Conditional Response - Path Starting-point
+    //Conditional Responses
     if (path === "/") {
       response = "HTTP/1.1 200 OK\r\n\r\n";
     }
-    //Conditional Response - Sub-paths
-    if (path.startsWith("/echo/") && method === "GET") {
-      const path_str = path.slice("/echo/".length);
-      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${path_str.length}\r\n\r\n${path_str}`;
+    if (path.startsWith("/echo/")) {
+      const str = path.slice("/echo/".length);
+      // console.log(path);
+      // console.log(str);
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`;
+    }
 
-      // Testing for User-Agent Header
-    } else if (path === "/user-agent" && method === "GET") {
-      const path_str = request
+    // Testing for User-Agent
+    if (path === "/user-agent") {
+      const str = request
         .split("\r\n")
         .find((el) => el.toLowerCase().includes("user-agent:"))!
         .slice("user-agent:".length)
         .trim();
-      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${path_str.length}\r\n\r\n${path_str}`;
+      response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`;
+    }
 
-      // Testing for Files - Reading Method
-    } else if (path.startsWith("/files/") && method === "GET") {
+    // Testing for Files
+    if (path.startsWith("/files/")) {
       const fileName = path.slice("/files/".length).trim();
-      // const fileSize = statSync('tmp/'+fileName).size;
-      try {
-        const fileContent = await readFile(process.argv[3] + fileName);
-        response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`;
-      } catch {
-        response = `HTTP/1.1 404 Not Found\r\n\r\n`;
-      }
-      // Testing for Files - Writing Method
-    } else if (path.startsWith("/files/") && method === "POST") {
-      console.log("hello");
-      response = `HTTP/1.1 201 Created\r\n\r\n`;
+      const fileSize = statSync(process.argv[3] + fileName).size;
+      const fileContent = readFileSync(process.argv[3] + fileName);
+      response = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileSize}\r\n\r\n${fileContent}`;
     }
 
     socket.write(response);
@@ -61,6 +55,7 @@ const server = net.createServer((socket) => {
 console.log("Logs from your program will appear here!");
 
 // Initial Stage
+
 server.listen(4221, "localhost", () => {
   console.log("Server is running on port 4221");
 });
